c52092e441c82c0a0d07ad55b135f2de45a80e95
Doctor Mew <trc379@gmail.com>
2019-01-23 19:47:23 -0800

    asdsa

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,33 @@
-const db = require("../models");
+const dbUser = require("../models/user");
 
 // Defining methods for the booksController
 module.exports = {
   findUserbyUsername: function(req, res) {
-    db.User
+    dbUser
       .find(req.params.username)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.User
+    dbUser
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.User
+    dbUser
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.User
+    dbUser
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.User
+    dbUser
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
